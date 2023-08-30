d16a4f7ba137afba23d03bd7c496999d8e03a74c
DiogoKitahara <diogokitahara@gmail.com>
2023-08-30 20:20:53 -0300

    added

---
+++ 1_Periodo/Pensamento_Computacional/Lista_de_exercicios_4/exercicio_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+    // Declarar variaveis
+    // Armazenar idade, sexo, salario base, ano contratação e matricula
+    // Se idade >= 18
+        // Se M, 10% de reajuste
+        // Se F, 8% de reajuste
+    // Senão se idade >= 40
+        // Se M, 8% de reajuste
+        // Se F, 10% de reajuste
+    // Senão se idade >= 70
+        // Se M, 15% de reajuste
+        // Se F, 17% de reajuste
+}

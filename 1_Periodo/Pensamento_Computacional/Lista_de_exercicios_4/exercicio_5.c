8bcd08250753a9a5f8014a70e57b2bf7202ee09b
unknown <diogokitahara@gmail.com>
2023-08-31 20:18:16 -0300

    added

--- 1_Periodo/Pensamento_Computacional/Lista_de_exercicios_4/exercicio_5.c
+++ 1_Periodo/Pensamento_Computacional/Lista_de_exercicios_4/exercicio_5.c
@@ -3,10 +3,28 @@
 main() {
     // Declarar variaveis
     int idade, ano_contratacao, matricula;
+    float salario_base, salario_final;
+    char sexo;
     // Armazenar idade, sexo, salario base, ano contratação e matricula
+    printf("Idade: \n");
+    scanf("%i", &idade);
+    printf("Sexo: \n");
+    fflush(stdin);
+    scanf("%c", &sexo);
+    printf("Ano contratacao: \n");
+    scanf("%i", &ano_contratacao);
+    printf("Matricula: \n");
+    scanf("%i", &matricula);
+    printf("Salario: \n");
+    scanf("%f", &salario_base);
     // Se idade >= 18 && <= 39
+    if (idade >= 18 && idade <= 39) {
         // Se M, 10% de reajuste
+        if (sexo == 'M'){
+            if ()
+        }
         // Se F, 8% de reajuste
+    }
     // Senão se idade >= 40
         // Se M, 8% de reajuste
         // Se F, 10% de reajuste

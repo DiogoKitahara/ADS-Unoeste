21d7c6cb1a948a735bf1e03a9572635aa7fdfebe
DiogoKitahara <diogokitahara@gmail.com>
2023-09-22 19:02:10 -0300

    added

--- 1_Periodo/Pensamento_Computacional/Lista_de_exercicios_1/1_Calcular_idade.c
+++ 1_Periodo/Pensamento_Computacional/Lista_de_exercicios_1/1_Calcular_idade.c
@@ -2,33 +2,14 @@
 #include <ctype.h>
 
 main() {
-// Declarar variáveis
-char categoria, situacao, continuar;
-float preco;
-// Loop
-do {
-    // Armazenar preco
-    printf("Preco: ");
-    scanf("%f", &preco);
-    // Armazenar categoria e deixar maiusculo
-    printf("1 - Limpeza\n");
-    printf("2 - Alimentacao\n");
-    printf("3 - Vestuario\n");
-    printf("Categoria: ");
-    fflush(stdin);
-    scanf("%c", &categoria);
-    categoria = toupper(categoria);
-    // Armazenar situacao e deixar maiusculo
-    printf("Situacao [: ");
-    fflush(stdin);
-    scanf("%c", &situacao);
-    categoria = toupper(categoria);
-    
-    
-    // Armazenar resposta e deixar maiusculo
-    printf("Deseja continuar [S para sim / N para nao]? ");
-    fflush(stdin);
-    scanf("%c", &continuar);
-    continuar = toupper(continuar);
-    }
+    // Pegar o ano de nascimento da pessoa e o ano atual
+    int ano_nascimento, ano_atual;
+    printf("Ano de nascimento = ");
+    scanf("%d", &ano_nascimento);
+    printf("Ano atual = ");
+    scanf("%d", &ano_atual);
+    // Calcular sua idade
+    int idade = ano_atual - ano_nascimento;
+    // Printar sua idade
+    printf("Idade = %i\n", idade);
 }

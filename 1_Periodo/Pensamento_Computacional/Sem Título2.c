261ef638a1aa7e22134afacef66ff46a90359f35
DiogoKitahara <diogokitahara@gmail.com>
2023-10-11 21:49:29 -0300

    added

--- 1_Periodo/Pensamento_Computacional/Sem Título2.c
+++ 1_Periodo/Pensamento_Computacional/Sem Título2.c
@@ -3,8 +3,8 @@
 
 main() {
 	// Declarar variaveis
-	int laboratorio, micro, ra, menor_ra, tempo, menor_tempo = 99999, contador_aluno = 0, contador_feminino = 0, contador_masculino = 0;
-	float porcentagem_mulher;
+	int laboratorio, micro, ra, menor_ra, tempo, menor_tempo = 99999, contador_aluno = 0, contador_feminino = 0, contador_masculino = 0, mais_alunos = 0, mais_laboratorio;
+	float porcentagem_mulher, porcentagem_homem;
 	char sexo;
 	// Armazenar numero do laboratorio
 	printf("Numero do laboratorio: ");
@@ -12,6 +12,7 @@ main() {
 	// Loop para laboratorio
 	while(laboratorio > 0){
 		// Resetar a porcentagem de mulher
+		porcentagem_mulher = 0;
 		// Armazenar micro
 		printf("Numero do micro: ");
 		scanf("%d", &micro);
@@ -41,16 +42,28 @@ main() {
 				menor_tempo = tempo;
 				menor_ra = ra;
 			}
+			printf("--------------------------------------------------------------------\n");
 			printf("Numero do micro: ");
 			scanf("%d", &micro);
 		}
+		// Ver qual laboratório possui mais alunos
+		if (contador_aluno > mais_alunos){
+			mais_alunos = contador_aluno;
+			mais_laboratorio = laboratorio;
+		}
+		contador_aluno = 0;
 		// Porcentagem de mulheres em cada laboratorio
 		porcentagem_mulher = (float) contador_feminino / contador_aluno * 100;
-		printf("Porcentagem de mulher: %.2f\n", porcentagem_feminino);
+		printf("Porcentagem de mulher: %.2f\n", porcentagem_mulher);
+		printf("Aluno com menos tempo no micro: %d\n", menor_ra);
+		printf("--------------------------------------------------------------------\n");
 		// Armazenar numero do laboratorio
 		printf("Numero do laboratorio: ");
 		scanf("%d", &laboratorio);
 	}
-	// Ver qual laboratório possui mais alunos
 	// Porcentagem de homens entre todos os laboratorios
+	porcentagem_homem = (float) contador_masculino / contador_aluno * 100;
+	printf("Porcentagem de homem: %.2f\n", porcentagem_homem);
+	// Imprimir resultado
+	printf("Laboratorio com mais alunos: %d\n", mais_laboratorio);
 }
